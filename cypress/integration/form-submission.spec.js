86ebb2a1c7814a805a060fc71d11898de9281448
Celso Henrique <celso.henrique@hotmail.com.br>
2020-10-16 18:52:55 -0300

    Adding error form stub

--- cypress/integration/form-submission.spec.js
+++ cypress/integration/form-submission.spec.js
@@ -28,4 +28,30 @@ describe('Form input', () => {
 
     cy.get('.task-wrapper').should('have.length', 5);
   });
+
+  it('Shows error message for a failed submission', () => {
+    const newTodo = 'Buy pizza';
+
+    cy.server();
+    cy.route({
+      method: 'POST',
+      url: '/ToDoModels',
+      status: 500,
+      response: {}
+    }).as('save');
+
+    cy.seedAndVisit();
+
+    cy.get('.new-todo')
+      .type(newTodo)
+      .type('{enter}');
+
+    cy.wait('@save');
+
+    cy.get('.task-wrapper').should('have.length', 4);
+
+    cy.on('window:alert', text => {
+      expect(text).to.contains('500');
+    });
+  });
 });
